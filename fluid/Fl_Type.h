ca34c6c165592b6869e814aa49efca7383fa9263
Timothy Lee <timothy.ty.lee@gmail.com>
2021-05-12 23:37:53 +1000

    Fix Fl_Type.h by forward-declaring friend function with default value

--- fluid/Fl_Type.h
+++ fluid/Fl_Type.h
@@ -41,10 +41,12 @@
 
 void set_modflag(int mf);
 
+Fl_Widget *make_type_browser(int,int,int,int,const char *l=0);
+
 class Fl_Type {
 
   friend class Widget_Browser;
-  friend Fl_Widget *make_type_browser(int,int,int,int,const char *l=0);
+  friend Fl_Widget *make_type_browser(int,int,int,int,const char *);
   friend class Fl_Window_Type;
   virtual void setlabel(const char *); // virtual part of label(char*)
 
